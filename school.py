d20339bf34f9a1c6de2f740ba536941ba5e8fe07
cassien babey <cassien98@yahoo.fr>
2022-10-14 11:43:20 +0200

    yes sir

--- school.py
+++ school.py
@@ -1,12 +1,13 @@
 # pylint:disable=C0111,C0103
 import sqlite3
-
 conn = sqlite3.connect('data/school.sqlite')
 db = conn.cursor()
 
+
+
 def students_from_city(db, city):
     """return a list of students from a specific city"""
-    query = f"""SELECT *
+    query = """SELECT *
                 FROM students s
                 WHERE birth_city = ?
             """
@@ -14,15 +15,11 @@ def students_from_city(db, city):
     sortie = db.fetchall()
     return sortie
 
-print(students_from_city(db, 'London'))
+print(students_from_city(db, 'Paris'))
+
 
 
 
 # To test your code, you can **run it** before running `make`
 #   => Uncomment the following lines + run:
 #   $ python school.py
-#
-# import sqlite3
-# conn = sqlite3.connect('data/school.sqlite')
-# db = conn.cursor()
-# print(students_from_city(db, 'Paris'))
